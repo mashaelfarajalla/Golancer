586be3d3faa65a67c6c618d06fd706fc11195f37
mashael farajallah <mashael.qwer@gmail.com>
2023-08-26 12:24:23 +0300

    update

---
+++ assets/js/App.js
@@ -0,0 +1,120 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 5) {
+      $(".navbar").addClass("scrolled");
+    } else {
+      $(".navbar").removeClass("scrolled");
+    }
+  });
+});
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  centeredSlides: false,
+  slidesPerView: "auto",
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      slidesPerGroup: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      slidesPerGroup: 2,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      slidesPerGroup: 2,
+    },
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+      slidesPerGroup: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".mySwiper2", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  slidesPerView: 1,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
+  centerSlide: true,
+  fade: true,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      slidesPerGroup: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 15,
+      slidesPerGroup: 1,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+      slidesPerGroup: 2,
+    },
+    991: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      slidesPerGroup: 4,
+    },
+  },
+});
+
+var swiper = new Swiper(".mySwiper3", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  spaceBetween: 110,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+});
+
+var btnContainer = document.getElementById("myul");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav-link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
